242a5ad4196f00b0c4df68c8411d65f867075200
Heder <hedersafe.2016@gmail.com>
2025-04-13 18:56:30 -0300

    Agregue en la funcion buscarNombreClave

--- MostrarPersonas.h
+++ MostrarPersonas.h
@@ -8,6 +8,17 @@ void mostrarNombres(char *persona[], int n){
         printf("%s", persona[i]);
     }
 }
+char *BuscarNombreClave(char *persona[], int n, char *nombre){
+    for (int i = 0; i < n; i++)
+    {
+        if (strstr(persona[i], nombre))
+        {
+            return persona[i];
+        }
+        
+    }
+    return NULL;
+}
 void liberarMemoria(char *persona[], int n){
     for (int i = 0; i < n; i++)
     {
